f3bbdd92b0b81b8081a13802a62dbdecb07f56d9
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-12-06 11:19:37 +0530

    Making the routes for the Booking Form component displaying using react router do instead of useState and open function.

--- bookingFormFullStack/frontend/frontend/src/App.js
+++ bookingFormFullStack/frontend/frontend/src/App.js
@@ -1,11 +1,16 @@
 /* src/App.js */
 import Home from "./Home";
+import BookingForm from "./BookingForm";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/BookingForm" element={<BookingForm />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- bookingFormFullStack/frontend/frontend/src/Home.js
+++ bookingFormFullStack/frontend/frontend/src/Home.js
@@ -1,40 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { Button, Container, Col, Row, Table } from "react-bootstrap";
+import { Button, Container, Table } from "react-bootstrap";
 import BookingForm from "./BookingForm";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Home() {
-  const [showBookingForm, setShowBookingForm] = useState(false);
+  // const [showBookingForm, setShowBookingForm] = useState(false);
 
   const [data, setData] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:3000/")
-      .then((res) => console.log(res.data))
+      .then((res) => {
+        setData(res.data); 
+        console.log(res.data)
+      })
       .catch((err) => console.log(err));
   }, []);
 
-  const openBookingForm = () => {
-    setShowBookingForm(true);
-  };
+  // const openBookingForm = () => {
+  //   setShowBookingForm(true);
+  // };
 
   return (
     <div className="bg-dark vh-100 d-flex align-items-center justify-content-center">
       <Container className="bg-light border rounded p-3 text-center">
-        <Row className="mb-3">
-          <Col className="d-flex align-items-center">
+        <div className="mb-3">
+          <div className="d-flex align-items-center">
             <h3>Details:</h3>
-            <Button
-              variant="success"
-              onClick={openBookingForm}
-              className="ml-3"
-            >
-              Add User +
-            </Button>
-          </Col>
-        </Row>
+            <Link to="/BookingForm" className="btn btn-success btn-sm ml-3">
+      Add User +
+    </Link>
+          </div>
+        </div>
         <hr />
-        {showBookingForm && <BookingForm />}
+        {/* {showBookingForm && <BookingForm />} */}
         <Table striped bordered hover responsive>
           <thead>
             <tr>
@@ -42,18 +42,23 @@ function Home() {
               <th>Name</th>
               <th>Phone No.</th>
               <th>Email Id</th>
+              <th>Functionalities</th>
             </tr>
           </thead>
-          {/* <tbody>
+          <tbody>
             {data.map((item, index) => (
               <tr key={index}>
                 <td>{index + 1}</td>
-                <td>{item.name}</td>
+                <td>{item.username}</td>
                 <td>{item.phone}</td>
                 <td>{item.email}</td>
+                <td>
+                  <Button variant="primary" className="btn-sm mr-2">Update</Button>
+                  <Button variant="danger" className="btn-sm">Delete</Button>
+                </td>
               </tr>
             ))}
-          </tbody> */}
+          </tbody>
         </Table>
       </Container>
     </div>
