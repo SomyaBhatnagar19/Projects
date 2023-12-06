4cadcc8c070dade021fd1c450e06c6462ff9bd11
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-12-06 12:39:02 +0530

    Adding the post functionality to the booking form and also correcting the server to do the post request to the sql users table on sbmiting details in the form.

--- bookingFormFullStack/backend/server.js
+++ bookingFormFullStack/backend/server.js
@@ -1,20 +1,23 @@
-/* /backend/server.js */
-
-//importing files
+// Importing files
 const express = require('express');
-const mysql = require('mysql2'); // Import the mysql2 library
+const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
 
+// Use CORS middleware
 app.use(cors());
+// Use body parsing middleware for JSON data 
+// I'm adding the below two lines to correct the post request error 404
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Create a MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost', 
-  user: 'root', 
-  password: 'Somya@1901b', 
-  database: 'node-complete' 
+  host: 'localhost',
+  user: 'root',
+  password: 'Somya@1901b',
+  database: 'node-complete',
 });
 
 // Connect to MySQL
@@ -28,7 +31,7 @@ connection.connect((err) => {
 
 // Define a route to fetch data from the 'users' table
 app.get('/', (req, res) => {
-  const query = 'SELECT * FROM users'; 
+  const query = 'SELECT * FROM users';
 
   // Execute the query
   connection.query(query, (err, results) => {
@@ -41,10 +44,27 @@ app.get('/', (req, res) => {
   });
 });
 
+// Define a route to post data from the form to the 'users' table
+app.post('/BookingForm', (req, res) => {
+  const query = "INSERT INTO users (username, phone, email) VALUES (?, ?, ?)";
+  const values = [
+    req.body.username,
+    req.body.phone,
+    req.body.email,
+  ];
+
+  connection.query(query, values, (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json("BookingForm");
+    }
+  });
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
--- bookingFormFullStack/frontend/frontend/src/BookingForm.js
+++ bookingFormFullStack/frontend/frontend/src/BookingForm.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function BookingForm() {
+
+    const [username, setUsername] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmitForm = (e) => {
+      e.preventDefault();
+      axios
+      .post("http://localhost:3000/BookingForm", { username, phone, email })
+      .then(res => {
+        navigate('/');
+      })
+      .catch(err => console.log(err))
+    }
+
   return (
+
     <div className="bg-dark vh-100 d-flex align-items-center justify-content-center">
       <Container className="bg-light mt-5 mb-5 border rounded p-4">
         <h5 className="text-center mb-4">Booking Appointment Form</h5>
         <hr />
-        <Form>
+        <Form onSubmit={handleSubmitForm}>
           <Row className="mb-3">
             <Col>
               <Form.Group controlId="username">
                 <Form.Label>Username:</Form.Label>
-                <Form.Control type="text" placeholder="Enter your username" />
+                <Form.Control type="text" placeholder="Enter your username" 
+                  onChange={e => setUsername(e.target.value)}
+                />
               </Form.Group>
             </Col>
           </Row>
@@ -24,6 +46,7 @@ function BookingForm() {
                 <Form.Control
                   type="tel"
                   placeholder="Enter your phone number"
+                  onChange={e => setPhone(e.target.value)}
                 />
               </Form.Group>
             </Col>
@@ -33,7 +56,9 @@ function BookingForm() {
             <Col>
               <Form.Group controlId="emailid">
                 <Form.Label>Email Id:</Form.Label>
-                <Form.Control type="email" placeholder="Enter your email" />
+                <Form.Control type="email" placeholder="Enter your email" 
+                   onChange={e => setEmail(e.target.value)}  
+                />
               </Form.Group>
             </Col>
           </Row>
