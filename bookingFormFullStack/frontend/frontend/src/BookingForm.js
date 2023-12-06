7f9302f3cda4393d923014be6a73a1fd9d5969ec
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-12-06 11:35:27 +0530

    Adding styling to the booking form file.

--- bookingFormFullStack/frontend/frontend/src/BookingForm.js
+++ bookingFormFullStack/frontend/frontend/src/BookingForm.js
@@ -3,47 +3,51 @@ import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 function BookingForm() {
   return (
-    <Container>
-      <h5 className="mb-4">Booking Appointment Form:</h5>
-      <Form>
-        <Row className="mb-3">
-          <Col>
-            <Form.Group controlId="username">
-              <Form.Label>Username:</Form.Label>
-              <Form.Control type="text" placeholder="Enter your username" />
-            </Form.Group>
-          </Col>
-        </Row>
-
-        <Row className="mb-3">
-          <Col>
-            <Form.Group controlId="phone">
-              <Form.Label>Phone No.:</Form.Label>
-              <Form.Control type="number" placeholder="Enter your phone number" />
-            </Form.Group>
-          </Col>
-        </Row>
-
-        <Row className="mb-3">
-          <Col>
-            <Form.Group controlId="emailid">
-              <Form.Label>Email Id:</Form.Label>
-              <Form.Control type="email" placeholder="Enter your email" />
-            </Form.Group>
-          </Col>
-        </Row>
-
-        <Row className="justify-content-end">
-          <Col xs={3}>
-            <Button variant="success" type="submit" block>
-              Submit
-            </Button>
-          </Col>
-        </Row>
-
+    <div className="bg-dark vh-100 d-flex align-items-center justify-content-center">
+      <Container className="bg-light mt-5 mb-5 border rounded p-4">
+        <h5 className="text-center mb-4">Booking Appointment Form</h5>
         <hr />
-      </Form>
-    </Container>
+        <Form>
+          <Row className="mb-3">
+            <Col>
+              <Form.Group controlId="username">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control type="text" placeholder="Enter your username" />
+              </Form.Group>
+            </Col>
+          </Row>
+
+          <Row className="mb-3">
+            <Col>
+              <Form.Group controlId="phone">
+                <Form.Label>Phone No.:</Form.Label>
+                <Form.Control
+                  type="tel"
+                  placeholder="Enter your phone number"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+
+          <Row className="mb-3">
+            <Col>
+              <Form.Group controlId="emailid">
+                <Form.Label>Email Id:</Form.Label>
+                <Form.Control type="email" placeholder="Enter your email" />
+              </Form.Group>
+            </Col>
+          </Row>
+          <hr />
+          <Row className="justify-content-end">
+            <Col xs={3}>
+              <Button variant="success" type="submit" block>
+                Add Appointment
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    </div>
   );
 }
 
--- bookingFormFullStack/frontend/frontend/src/Home.js
+++ bookingFormFullStack/frontend/frontend/src/Home.js
@@ -1,26 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { Button, Container, Table } from "react-bootstrap";
-import BookingForm from "./BookingForm";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 function Home() {
-  // const [showBookingForm, setShowBookingForm] = useState(false);
 
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3000/")
+    axios
+      .get("http://localhost:3000/")
       .then((res) => {
-        setData(res.data); 
-        console.log(res.data)
+        setData(res.data);
+        console.log(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
-
-  // const openBookingForm = () => {
-  //   setShowBookingForm(true);
-  // };
 
   return (
     <div className="bg-dark vh-100 d-flex align-items-center justify-content-center">
@@ -28,17 +23,16 @@ function Home() {
         <div className="mb-3">
           <div className="d-flex align-items-center">
             <h3>Details:</h3>
-            <Link to="/BookingForm" className="btn btn-success btn-sm ml-3">
-      Add User +
-    </Link>
+            <Link to="/BookingForm" className="btn btn-success ml-3 d-flex ms-auto">
+              Add
+            </Link>
           </div>
         </div>
         <hr />
-        {/* {showBookingForm && <BookingForm />} */}
         <Table striped bordered hover responsive>
           <thead>
             <tr>
-              <th>#</th>
+              <th>S.No.</th>
               <th>Name</th>
               <th>Phone No.</th>
               <th>Email Id</th>
@@ -53,8 +47,12 @@ function Home() {
                 <td>{item.phone}</td>
                 <td>{item.email}</td>
                 <td>
-                  <Button variant="primary" className="btn-sm mr-2">Update</Button>
-                  <Button variant="danger" className="btn-sm">Delete</Button>
+                  <Button variant="primary" className="btn-sm mr-2">
+                    Update
+                  </Button>
+                  <Button variant="danger" className="btn-sm">
+                    Delete
+                  </Button>
                 </td>
               </tr>
             ))}
